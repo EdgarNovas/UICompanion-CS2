97761d466bbd19c74dd4fdfa1b349179886b2fa2
NilBG <nil.barrero@estudiant.enti.cat>
2025-12-16 11:20:05 +0100

    Fragments

--- app/src/main/java/com/example/apirest/ToolbarFragment.kt
+++ app/src/main/java/com/example/apirest/ToolbarFragment.kt
@@ -1,4 +1,4 @@
-package com.example.tuapp
+package com.example.apirest
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,8 +7,34 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TextView
+import com.example.apirest.R
 
 class ToolbarFragment : Fragment() {
 
+    private lateinit var toolbarTitle: TextView
+    private lateinit var menuButton: ImageButton
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_toolbar, container, false)
+
+        toolbarTitle = view.findViewById(R.id.toolbar_title)
+        menuButton = view.findViewById(R.id.btn_menu)
+
+        return view
+    }
+
+    fun setToolbarTitle(title: String) {
+        if (::toolbarTitle.isInitialized) {
+            toolbarTitle.text = title
+        }
+    }
+
+    fun setMenuButtonAction(listener: View.OnClickListener) {
+        if (::menuButton.isInitialized) {
+            menuButton.setOnClickListener(listener)
+        }
+    }
 }
