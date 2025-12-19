4c6c2b75bdc4d6b4ad7fc20f235e70dab2b9dacb
EdgarNovas <edgar.novas@enti.cat>
2025-12-20 00:31:04 +0100

    SteamAvatar Now Appears on Profile

--- app/src/main/java/com/example/apirest/MainMenu.kt
+++ app/src/main/java/com/example/apirest/MainMenu.kt
@@ -3,12 +3,14 @@ package com.example.apirest
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 
 class MainMenu : AppCompatActivity() {
     private lateinit var btnCloseSesion : Button
@@ -24,38 +26,31 @@ class MainMenu : AppCompatActivity() {
     }
 
 
-    private fun crearPerfil()
-    {
-        // Abrimos el archivo de preferencias
+    private fun crearPerfil() {
+        //Abrimos el archivo de preferencias
         val sharedPref = getSharedPreferences("MisDatosSteam", MODE_PRIVATE)
 
-        //Leemos los datos (el segundo parámetro es el valor por defecto si no encuentra nada)
-        val mySteamId = sharedPref.getString("steam_id", null)
         val mySteamName = sharedPref.getString("steam_name", "Usuario Desconocido")
+        val mySteamAvatarUrl = sharedPref.getString("steam_avatar", null) // Recuperamos la URL
+
 
         val txtNombreCompleto = findViewById<TextView>(R.id.text_name)
-        val txtInicial = findViewById<TextView>(R.id.user_initial)
 
-        if (mySteamId != null) {
-            // Poner el nombre
-            txtNombreCompleto.text = mySteamName
-
-            // B. Poner solo la primera letra (Logica segura)
-            if (!mySteamName.isNullOrEmpty()) {
-                // Cogemos la primera letra la pasamos a String
-                val letra = mySteamName[0].toString().uppercase()
-                txtInicial.text = letra
-            } else {
-                txtInicial.text = "?" // Por si acaso viene vacío
-            }
-            //Quitado por los nombres
-            // toolbarFragment.setToolbarTitle(mySteamName)
+        val imgAvatar = findViewById<ImageView>(R.id.user_avatar_image)
 
 
-            Toast.makeText(this, "Hola de nuevo, $mySteamName", Toast.LENGTH_SHORT).show()
-        } else {
-            // No hay nadie logueado, quizás deberías enviarlo al Login
+        txtNombreCompleto.text = mySteamName
+
+
+        if (mySteamAvatarUrl != null) {
+            Glide.with(this)
+                .load(mySteamAvatarUrl) // La URL que guardaste
+                .placeholder(R.drawable.ic_launcher_foreground) // Imagen mientras carga
+                .error(R.drawable.ic_launcher_foreground) // Imagen si falla
+                .into(imgAvatar) // Donde ponerla
         }
+
+        Toast.makeText(this, "Hola de nuevo, $mySteamName", Toast.LENGTH_SHORT).show()
     }
 
     private fun cerrarSesion() {
@@ -65,7 +60,7 @@ class MainMenu : AppCompatActivity() {
         editor.clear()
         editor.apply()
 
-        // Redirigir al usuario a la pantalla de Login de nuevo
+
         val intent = Intent(this, SteamLogin::class.java)
         startActivity(intent)
         finish()
